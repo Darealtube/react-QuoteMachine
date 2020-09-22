a3f400146333ae562ccccbb00aeb79acc5aacd5f
Darealtube <evajavier143@gmail.com>
2020-09-22 09:13:28 +0800

    Updates

---
+++ precache-manifest.47b1a3cb197ee990bddea55122855831.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c0d81efff2d30febadbd215ec57cb5",
+    "url": "/react-QuoteMachine/index.html"
+  },
+  {
+    "revision": "85e1d75c9f8fa69a7f63",
+    "url": "/react-QuoteMachine/static/css/main.d26d7cba.chunk.css"
+  },
+  {
+    "revision": "3ce9d4a019513d1a8b8a",
+    "url": "/react-QuoteMachine/static/js/2.cbcd5c49.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/react-QuoteMachine/static/js/2.cbcd5c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85e1d75c9f8fa69a7f63",
+    "url": "/react-QuoteMachine/static/js/main.e95a6ae0.chunk.js"
+  },
+  {
+    "revision": "8fe58cff3ed8ff4ab03e",
+    "url": "/react-QuoteMachine/static/js/runtime-main.953a5b9f.js"
+  }
+]);
